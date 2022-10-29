b0bf57c8fdd3037f50ca8d36dd212e82daec50ed
Pedro <pedrohvidals@gmail.com>
2022-10-29 11:04:32 -0300

    Update main.ino

---
+++ main.ino
@@ -0,0 +1,84 @@
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+
+#include <MFRC522.h>
+#include <SPI.h>
+
+#define SS_PIN 21
+#define RST_PIN 22
+#define ID "6A A9 B0 A3"
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+int acionador = 15;
+ 
+const char* ssid = "PEDRO_E_IGOR 2G";
+const char* password =  "46421148";
+ 
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
+ 
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+  if(type == WS_EVT_CONNECT){
+      Serial.println("Websocket client connection received");}      
+   
+  else if(type == WS_EVT_DISCONNECT){
+      Serial.println("Client disconnected");
+  }
+}
+ 
+void setup(){
+  Serial.begin(115200);
+
+  SPI.begin(); //iniciando spi
+  rfid.PCD_Init(); //iniciando rfid
+
+  pinMode(acionador, OUTPUT);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println(WiFi.localIP());
+ 
+  ws.onEvent(onWsEvent);
+  server.addHandler(&ws);
+ 
+  server.begin();
+}
+ 
+void loop(){
+  if (! rfid.PICC_IsNewCardPresent()){ //checando se existe algum cartao no leitor
+    return;
+  }
+    
+  if (! rfid.PICC_ReadCardSerial()){ //vendo c da pra ler o cartao
+    return;
+  }
+      
+  String id_cartao = "";
+  byte i;
+        
+  for (byte i = 0; i < rfid.uid.size; i++){
+    Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(rfid.uid.uidByte[i], HEX);
+    id_cartao.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    id_cartao.concat(String(rfid.uid.uidByte[i], HEX));
+  }
+    
+  id_cartao.toUpperCase();
+    
+  if(id_cartao.substring(1) == ID){
+    Serial.println("Acesso liberado");
+    digitalWrite(acionador, HIGH);
+    ws.textAll(id_cartao.substring(1));
+  }
+      
+  else{
+    Serial.println("Acesso Negado");
+    ws.textAll(id_cartao.substring(1));
+  }
+  delay(1000);
+}
